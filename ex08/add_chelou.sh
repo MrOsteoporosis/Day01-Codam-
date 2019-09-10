281011155f948b31d539f91665390a38833f921e
Andreas Verheijen <averheij@student.codam.nl>
2019-09-10 16:05:30 +0200

    stuck on exercise 08 left some urls for later in the shell file

---
+++ ex08/add_chelou.sh
@@ -0,0 +1,4 @@
+echo "obase=mrdoc; ibase=’\"?!; FT_NBR1"|bc -l
+
+https://unix.stackexchange.com/questions/296141/how-to-use-a-special-character-as-a-normal-one
+https://www.commandlinefu.com/commands/view/1674/base-conversions-with-bc
